c3b654a0e7e1f9df3380c3cc8276179c0d62ebe0
Lorenzo Barberi <84438254+Sbro03@users.noreply.github.com>
2022-11-21 10:40:27 +0100

    Add files via upload

---
+++ Programmazione 1/Laboratori/Lezione 07/Lorenzo/TestRicorsione.java
@@ -0,0 +1,12 @@
+public class TestRicorsione{
+	public static void main(String[] args){
+		//System.out.print(MetodiRicorsivi.contaCarattereW("Cretino", 'e'));
+		//TestVocali.exe();
+		String str = "onorarono";
+		//TestPalindromi.exe();
+		//ConvertiStringhe.exe();
+		//TestInversioneStringa.exe();
+		//System.out.println(MetodiRicorsivi.eliminaCarattere(str,5));
+		TestPermutazioni.exe();
+	}
+}
